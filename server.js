e599f621ea4981df2d0168e20886b2198b8a3d0d
Manpreet Kaur <mk747@student.le.ac.uk>
2024-02-03 13:42:24 +0000

    Added the server settings and connection to DB (local)

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = require("./app");
+
+dotenv.config({ path: "./config.env" });
+
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful"))
+  .catch((err) => console.error("DB connection error: ", err));
+
+const port = 3000 || process.env.PORT;
+
+const server = app.listen(3000, "localhost", () => {
+  console.log(`Server is running on port ${port}`);
+});
