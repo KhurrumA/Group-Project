ec04abc5c95b47e7f1c184117a173300adca13e7
Manpreet Kaur <mk747@student.le.ac.uk>
2024-02-03 15:08:04 +0000

    Added connection ATLAS DB

--- server.js
+++ server.js
@@ -11,16 +11,31 @@ process.on("uncaughtException", (err) => {
 });
 
 dotenv.config({ path: "./config.env" });
+//CONNECTING TO ATLAS DB
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
 
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("DB connection successful"))
-  .catch((err) => console.error("DB connection error: ", err));
+  .then(() => console.log("DB connection successful!"));
+
+//CONNECTING TO LOCAL DB
+// mongoose
+//   .connect(process.env.DATABASE_LOCAL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log("DB connection successful"))
+//   .catch((err) => console.error("DB connection error: ", err));
 
 const port = 3000 || process.env.PORT;
 
