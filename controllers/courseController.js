df85aa69653811c3d4dbac2770b18d8a4d38e6a6
Zainab Patel <zainabpatel@Zainabs-MacBook-Pro.local>
2024-02-06 14:12:08 +0000

    update courseController

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,5 +1,5 @@
 const Course = require("../models/courseModel");
-const functionFactory = require("./functionHandlers");
+const functionFactory = require("./functionHandler");
 
 exports.getAllCourse = functionFactory.getAll(Course);
 exports.getCourse = functionFactory.getOne(Course);
