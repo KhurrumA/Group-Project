8fcae8b756565b22d4173cca7123d088e2879eaa
Zainab Patel <zainabpatel@Zainabs-MacBook-Pro.local>
2024-02-06 14:47:46 +0000

    added me route

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,6 @@
 //const userController = require("./../controllers/userController");
 const authController = require("./../controllers/authController");
+const pointsController = require("./../controllers/pointsController");
 const express = require("express");
 
 const router = express.Router();
@@ -7,5 +8,7 @@ const router = express.Router();
 router.post("/signup", authController.registerUser); //only post data so that new user can be created
 router.post("/login", authController.login);
 router.get("/logout", authController.logout); //logout route
+
+router.get("/me/:id", pointsController.addPoints);
 
 module.exports = router;
