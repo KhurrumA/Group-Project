4b0ae23b20814a4cf5b1d1f6f0fdf4504ad9b48c
Zainab Patel <zainabpatel@Zainabs-MacBook-Pro.local>
2024-02-06 13:56:00 +0000

    added 2 new function

--- controllers/functionHandler.js
+++ controllers/functionHandler.js
@@ -19,3 +19,34 @@ exports.updateOne = (Model) =>
       },
     });
   });
+
+//CREATE/POST Review/Course
+exports.createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body); //create new document with the data passed through the body
+
+    res.status(201).json({
+      status: "success",
+      data: {
+        data: doc,
+      },
+    }); //201 = created new resource
+  });
+
+// READING ALL Reviews/Course/Users
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    let filter = {};
+    if (req.params.courseId) filter = { course: req.params.courseId };
+
+    const doc = await Model.find(filter);
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: "success",
+      results: doc.length,
+      data: {
+        data: doc,
+      },
+    });
+  });
