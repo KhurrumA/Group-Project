f1e05a5c58931d271db4c12951ab62c551d2eb20
Manpreet Kaur <mk747@student.le.ac.uk>
2024-02-09 15:19:06 +0000

    added the comments

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,16 +1,13 @@
-//const userController = require("./../controllers/userController");
 const authController = require("./../controllers/authController");
 const pointsController = require("./../controllers/pointsController");
 const express = require("express");
 
 const router = express.Router();
 
-router.post("/signup", authController.registerUser); //only post data so that new user can be created
-router.post("/login", authController.login);
+router.post("/signup", authController.registerUser); //post data so that new user can be created
+router.post("/login", authController.login); //post data so we can authenticate the user
 router.get("/logout", authController.logout); //logout route
 
-
-router.get("/me/:id", authController.protect, pointsController.addPoints);
-
+router.get("/me/:id", authController.protect, pointsController.addPoints); //protecting the route so only the logged in user can see their points
 
 module.exports = router;
