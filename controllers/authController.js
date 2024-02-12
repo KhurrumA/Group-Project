904eea3fe0570baede907dca47d1a82a67e86fbc
Manpreet Kaur <mk747@student.le.ac.uk>
2024-02-12 13:47:29 +0000

    Added comments for readability

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,14 +2,9 @@ const User = require("./../models/userModel");
 const catchAsync = require("./../utils/catchAsync");
 const appError = require("./../utils/appError");
 const jwt = require("jsonwebtoken");
-const {promisify} = require ("util");
+const { promisify } = require("util");
 
 //SIGNUP TOKEN
-/*
-1)Get the token from the config file
-2) Use the .sign() on jwt and set the expire time to the one in the config file
-*/
-
 const signUpJwtToken = (id) => {
   return jwt.sign({ id }, process.env.JWT, {
     expiresIn: process.env.JWT_EXPIRY,
@@ -17,13 +12,6 @@ const signUpJwtToken = (id) => {
 };
 
 //CREATE JWT TOKEN
-/*
-1) Get the signup token
-2) Define the cookie for jwt
-3) Send the cookie back with the token to the cookie function on the res
-4) Send back the 
-*/
-
 const createJWT = (user, statusCode, res) => {
   const jwtToken = signUpJwtToken(user._id); //_id is unique for each user
 
@@ -44,7 +32,7 @@ const createJWT = (user, statusCode, res) => {
 
   res.status(statusCode).json({
     status: "success",
-    jwtToken, //sending back the json token
+    jwtToken, //sending back the json web token
     data: {
       user, //sending back the user data wihtout the password for security purpose
     },
@@ -52,12 +40,7 @@ const createJWT = (user, statusCode, res) => {
 };
 
 //SIGNUP
-//1)Create a varible and set the newUser data
-//2)Create and send the jwt to the user
-//3) Add app.use(express.json({ limit: "10kb" })); in app.js
-
 exports.registerUser = catchAsync(async (req, res, next) => {
-  //console.log(req.body);
   const regUser = await User.create({
     name: req.body.name,
     email: req.body.email,
@@ -68,53 +51,35 @@ exports.registerUser = catchAsync(async (req, res, next) => {
   });
 
   //sending the jwt token for the new user
-  //
   createJWT(regUser, 201, res);
 });
 
 //LOGIN
-/*
-Get the email password
-1) Check if the email and password exists
-2) Check if the user exists and if the password is correct
-3) If it is okay send back the jwt token
-*/
-
 exports.login = catchAsync(async (req, res, next) => {
   console.log(req.body);
   const { email, password } = req.body; //using the desctructring
 
   //if there is no email or password --> return error
-  //It takes as first argument a string (error message)
-  //second argument is the staust code 400 -> bad request
-  //1)
   if (!email || !password) {
     return next(appError("Please provide email and password!", 400));
   }
   //Getting the user that has the email specified by the user
   const user = await User.findOne({ email }).select("+password");
-  //2)
-  // console.log(user);
-  // console.log(await user.correctPassword("test", "test"));
+
+  //Check if the user exists or if the password is correct or not
   if (!user || !(await user.correctPassword(password, user.password))) {
     return next(new appError("Incorrect email or password!", 401)); //401 means unauthorized
   } else {
     console.log("you are logged");
   }
 
-  //3
+  //Create the JWT and send it back to the user
   createJWT(user, 200, res);
 });
 
 //LOGOUT
-/*
-This is done before setting the cookie to null or not a random value
-1) Get the cookie
-2) Set an expiry
-3) Send back the response: cookie and the status code
-*/
-
 exports.logout = (req, res) => {
+  //Sending back empty cookie
   res.cookie("jwt", "loggedout", {
     expiresIn: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
@@ -122,18 +87,20 @@ exports.logout = (req, res) => {
   res.status(200).json({ status: "success" });
 };
 
+//PROTECT
 exports.protect = catchAsync(async (req, res, next) => {
   //1)Get the token and check if it exists
   let token;
   if (
     req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
+    req.headers.authorization.startsWith("Bearer") //JWT is mostly in the header and starts with "Bearer"
   ) {
-    token = req.headers.authorization.split(" ")[1];
+    token = req.headers.authorization.split(" ")[1]; //getting the JWT
   } else if (req.cookies.jwt) {
-    token = req.cookies.jwt;
+    token = req.cookies.jwt; //If all previous case are not available get the JWT from the cookie
   }
 
+  //Check if the token exists
   if (!token) {
     return next(
       new appError("You are not logged in! Please login to get access", 401)
