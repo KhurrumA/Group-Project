d2c768e72c469b1f3ff36593f3df2aaa2c72f6aa
Khurrum Arif <ka382@student.le.a.uk>
2024-02-12 20:21:06 +0000

    adding code to review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -41,3 +41,70 @@ const reviewSchema = new mongoose.Schema(
 
 const Review = mongoose.model("Review", reviewSchema);
 module.exports = Review;
+
+
+//HANDLE DUPLICATE REVIEW
+reviewSchema.index({ course: 1, user: 1 }, { unique: true });
+
+//POPULATE Middleware
+//The pre word means that this will be done before the data is saved in DB
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "user",
+    select: "name",
+  });
+
+  next();
+});
+
+//CALCULATE AVERAGE RATING Middleware - Static methods
+//calcAverageRatings is the name given to the function
+
+reviewSchema.statics.calAverageRatings = async function (courseId) {
+  const stats = await this.aggregate([
+    {
+      //1)Select all the reviews that have the same course ID
+      $match: { course: courseId },
+    },
+    {
+      //Calculate the statistics and group the reviews by course
+      $group: {
+        _id: "$course",
+        nRating: { $sum: 1 }, //add 1 for each course matched
+        average: { $avg: "$rating" }, //name of the field where you want to calculate the average from
+      },
+    },
+  ]); //this points directly to the model and aggregate is always called on the model
+  console.log(stats);
+  if (stats.length > 0) {
+    await Course.findByIdAndUpdate(courseId, {
+      ratingsAverage: stats[0].average,
+      ratingsQuantity: stats[0].nRating,
+    });
+  } else {
+    await Course.findByIdAndUpdate(courseId, {
+      ratingsAverage: 0,
+      ratingsQuantity: 4.5,
+    });
+  }
+};
+
+reviewSchema.post("save", function () {
+  //this points to the current review constructor is the model that created that document
+  this.constructor.calAverageRatings(this.course);
+});
+
+//FindByIdAndUpdate/FindByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  //access the current review document
+  //created the property on revi
+  this.revi = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  //The query finished and review has been updated
+  //this.revi is as the first post
+  //await this.findOne(); does not work here, quert has already executed
+  await this.revi.constructor.calAverageRatings(this.revi.course);
+});
