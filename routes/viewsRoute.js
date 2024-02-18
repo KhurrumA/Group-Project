1aec02681c9d44d6829f7d5f85fd95264f477987
M Kaur <mk747@student.le.ac.uk>
2024-02-18 13:03:43 +0000

    Added new endpoints

--- routes/viewsRoute.js
+++ routes/viewsRoute.js
@@ -11,6 +11,11 @@ router.get("/courses", authController.protect, viewsController.getCourses);
 router.get("/course/:slug", authController.protect, viewsController.getCourse);
 router.get("/login", authController.isLoggedIn, viewsController.getLoginForm);
 router.get("/signup", viewsController.getSignupForm);
-router.get("/:slug/review", viewsController.postReview)
+router.get("/review/:slug", authController.protect, viewsController.postReview);
+router.get(
+  "/courseOverview/:slug",
+  authController.protect,
+  viewsController.getCourseOverview
+);
 
 module.exports = router;
