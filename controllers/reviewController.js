69a19783aa0b4cda679359a68946caff3fc3fa64
M Kaur <mk747@student.le.ac.uk>
2024-02-18 12:42:33 +0000

    Added post review and debugged

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -2,12 +2,15 @@ const Review = require("../models/reviewModel");
 const functionFactory = require("./functionHandler");
 
 exports.updateReview = functionFactory.updateOne(Review);
-exports.createReview = functionFactory.createOne(Review);
 exports.getAllReviews = functionFactory.getAll(Review);
 exports.getReview = functionFactory.getOne(Review);
-exports.setCourseUserIds = (req, res, next) => {
-  if (!req.body.course) req.body.course = req.params.courseId;
-  // if no used add the user id
-  if (!req.body.user) req.body.user = req.user.id;
-  next();
-};
+//POST REVIEW
+exports.postReview = catchAsync(async (req, res, next) => {
+  const regReview = await Review.create({
+    review: req.body.review,
+    rating: req.body.rating,
+    course: req.body.courseId,
+    user: req.user._id,
+  });
+  res.status(200).json({ status: "success", data: { regReview } });
+});
