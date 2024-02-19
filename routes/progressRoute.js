f6c8f502ba194c117562ec416ea08bdba1745494
M Kaur <mk747@student.le.ac.uk>
2024-02-19 22:38:32 +0000

    Updated routes

--- routes/progressRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authController = require("./../controllers/authController");
-const progressController = require('./../controllers/progressController')
-
-const router = express.Router();
-//Route for controller 
-router.post('/recordprogress', authController.protect, progressController.Progress);
-
-module.exports = router;
