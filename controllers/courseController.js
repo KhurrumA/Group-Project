d79d8d66713ae065a1a190c1586270105a18138e
M Kaur <mk747@student.le.ac.uk>
2024-02-21 14:19:59 +0000

    Revert "Merge branch 'master' into 'main'"
    This reverts merge request !4

--- controllers/courseController.js
+++
@@ -1,5 +0,0 @@
-const Course = require("../models/courseModel");
-const functionFactory = require("./functionHandler");
-
-exports.getAllCourse = functionFactory.getAll(Course);
-exports.getCourse = functionFactory.getOne(Course);
