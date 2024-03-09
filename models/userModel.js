0ecb1bb5981d1e420ba7f1e0bb248a5b351cb6bd
M Kaur <mk747@student.le.ac.uk>
2024-03-09 14:33:17 +0000

    Added photo field

--- models/userModel.js
+++ models/userModel.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema({
     enum: ["user", "admin"],
     default: "user",
   },
+  photo: {
+    type: String,
+    default: "default.jpg",
+  },
   password: {
     type: String,
     required: [true, "Please provide a password"],
