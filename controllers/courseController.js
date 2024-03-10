0ee41cda859e9cb3a046694f27b0a9b12e922979
David Sarkodie Junior <dojs1@student.le.ac.uk>
2024-03-10 18:16:35 +0000

    backend code to see the top 3 courses

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,3 +3,30 @@ const functionFactory = require("./functionHandler");
 
 exports.getAllCourse = functionFactory.getAll(Course);
 exports.getCourse = functionFactory.getOne(Course);
+
+exports.getTop3Courses = async (req, res) => {
+    try {
+      const topCourses = await Course.aggregate([
+        {
+          $project: {
+            name: 1, 
+            numberOfUsers: { $size: "$users" }, // Count the number of users enrolled
+          },
+        },
+        { $sort: { numberOfUsers: -1 } }, // Sort by numberOfUsers in descending order
+        { $limit: 3 }, // Limit to top 3
+      ]);
+  
+      res.status(200).json({
+        status: 'success',
+        data: {
+          courses: topCourses,
+        },
+      });
+    } catch (err) {
+      res.status(404).json({
+        status: 'fail',
+        message: err,
+      });
+    }
+  };
