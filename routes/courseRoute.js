2e45549381248c57a3865a8e06522f646e36ec94
M Kaur <mk747@student.le.ac.uk>
2024-03-12 20:13:20 +0000

    Added the stats route

--- routes/courseRoute.js
+++ routes/courseRoute.js
@@ -2,13 +2,24 @@ const express = require("express");
 const authController = require("../controllers/authController");
 const courseController = require("../controllers/courseController");
 const progressController = require("../controllers/progressController");
-const pointsController = require("../controllers/pointsController");
 
 const router = express.Router(); //creating the router
 
 router.get("/", authController.protect, courseController.getAllCourse); //home route
 
-router.get("/top-3-courses", authController.protect, courseController.getTop3Courses); // Get top 3 courses
+router.get(
+  "/top-3-courses",
+  authController.protect,
+  courseController.getTop3Courses
+); // Get top 3 courses
+//GET COURSES WITH RATING <=3
+router
+  .route("/stats")
+  .get(
+    authController.protect,
+    authController.restrictTo("admin"),
+    courseController.getStats
+  );
 
 router.get("/:id", authController.protect, courseController.getCourse); //get one course
 
