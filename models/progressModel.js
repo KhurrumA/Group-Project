f3c14e35207d271f4e9b0f44ff0cb2adbebe09e2
M Kaur <mk747@student.le.ac.uk>
2024-03-12 18:48:46 +0000

    Changed timeCompleted

--- models/progressModel.js
+++ models/progressModel.js
@@ -8,11 +8,8 @@ const progressSchema = new mongoose.Schema({
     type: Date,
     default: Date.now, // Automatically set to the current date and time when a new document is created
   },
-  timeCompleted: {
-    //field for time course is completed
-    type: Date,
-    default: Date.now,
-  },
+  timeCompleted: Date,
+
   user: {
     type: mongoose.Schema.ObjectId, // The ID of the user who clicked the link
     ref: "User", // References the User model
