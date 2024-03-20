9b239e01a0cdfe492f235a67d44f8c693a204184
M Kaur <mk747@student.le.ac.uk>
2024-03-20 09:26:47 +0000

    Updated package

--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const DB = process.env.DATABASE;
 
 mongoose
   .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+    //useNewUrlParser: true,
+    // useCreateIndex: true,
+    // useFindAndModify: false,
+    //useUnifiedTopology: true,
   })
   .then(() => console.log("DB connection successful!"))
   .catch((err) => console.error("DB connection error: ", err));
