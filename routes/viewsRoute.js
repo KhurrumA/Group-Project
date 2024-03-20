0c73dff76e620d32f53e01b06d809b57998887c5
M Kaur <mk747@student.le.ac.uk>
2024-03-20 21:44:45 +0000

    Added new routes

--- routes/viewsRoute.js
+++ routes/viewsRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 //RENDERING pug file
 router.get("/", authController.isLoggedIn, viewsController.getLanding);
+router.get("/account", authController.protect, viewsController.getAccount);
 router.get("/dashboard", authController.protect, viewsController.dashboard);
 router.get("/courses", authController.protect, viewsController.getCourses);
 router.get("/course/:slug", authController.protect, viewsController.getCourse);
@@ -16,6 +17,16 @@ router.get(
   "/ibmCourse/:slug",
   authController.protect,
   viewsController.getCourseOverview
+);
+router.get(
+  "/account/uploadPhoto",
+  authController.protect,
+  viewsController.getUploadPhoto
+);
+router.patch(
+  "/account/uploadPhoto",
+  authController.protect,
+  viewsController.uploadPhoto
 );
 
 module.exports = router;
